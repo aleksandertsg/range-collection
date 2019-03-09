e5d2d441a4161493b663578a83f992c09e75baad
aleksandertsg <aleksander.tsoganov@gmail.com>
2019-03-09 20:52:15 +0200

    task imported

---
+++ src/RangeCollection.ts
@@ -0,0 +1,34 @@
+// Task: Implement a 'Range Collection' class.
+// A pair of integers define a range, for example: [1, 5).
+// This range includes integers: 1, 2, 3, and 4.
+// A range collection is an aggregate of these ranges: [1, 5), [10, 11), [100, 201)
+
+/**
+ * RangeCollection class
+ * NOTE: Feel free to add any extra member variables/functions you like.
+ */
+export default class RangeCollection {
+  /**
+   * Adds a range to the collection
+   * @param {Array<number>} range - Array of two integers that specify beginning and end of range.
+   */
+  public add(range: [number, number]): void {
+    // TODO: implement this
+  }
+
+  /**
+   * Removes a range from the collection
+   * @param {Array<number>} range - Array of two integers that specify beginning and end of range.
+   */
+  public remove(range: [number, number]): void {
+    // TODO: implement this
+  }
+
+  /**
+   * Prints out the list of ranges in the range collection
+   */
+  public print(): string {
+    // TODO: implement this
+    return 'string'
+  }
+}
---
+++ tests/RangeCollection.spec.ts
@@ -0,0 +1,25 @@
+import 'mocha'
+import * as chai from 'chai'
+import RangeCollection from '../src/RangeCollection'
+
+const { expect } = chai
+
+describe('RangeCollection tests', () => {
+  describe('add', () => {
+    xit('example test', () => {
+      return
+    })
+  })
+
+  describe('remove', () => {
+    xit('example test', () => {
+      return
+    })
+  })
+
+  describe('print', () => {
+    xit('example test', () => {
+      return
+    })
+  })
+})
