d0e1043d5aa5185c073df8397fea507c2feea87a
aleksandertsg <aleksander.tsoganov@gmail.com>
2019-03-10 00:06:03 +0200

    RangeCollection with tests

--- src/RangeCollection.ts
+++ src/RangeCollection.ts
@@ -1,34 +1,49 @@
-// Task: Implement a 'Range Collection' class.
-// A pair of integers define a range, for example: [1, 5).
-// This range includes integers: 1, 2, 3, and 4.
-// A range collection is an aggregate of these ranges: [1, 5), [10, 11), [100, 201)
-
-/**
- * RangeCollection class
- * NOTE: Feel free to add any extra member variables/functions you like.
- */
 export default class RangeCollection {
-  /**
-   * Adds a range to the collection
-   * @param {Array<number>} range - Array of two integers that specify beginning and end of range.
-   */
+  private numbers: number[] = []
+
+  private createArrayInRange(start: number, end: number): number[] {
+    return Array.from({ length: (end - start) }, (_, i) => i + start)
+  }
+
+  private createRangeCollection (array: number[]): number[][] {
+    const rc: number[][] = []
+    let r: number[] = []
+
+    for (let i = 0; i < array.length; i++) {
+      if (r.length === 0) {
+        r.push(array[i])
+      } else if ((array[i] + 1) !== array[i + 1]) {
+        r.push(array[i])
+        rc.push([...r])
+
+        r = []
+      }
+    }
+
+    return rc
+  }
+
   public add(range: [number, number]): void {
-    // TODO: implement this
+    const newNumbers: number[] = [...this.numbers]
+
+    newNumbers.push(...this.createArrayInRange(range[0], range[1]))
+    newNumbers.sort((a: number, b: number) => a - b)
+
+    this.numbers = [...new Set(newNumbers)]
   }
 
-  /**
-   * Removes a range from the collection
-   * @param {Array<number>} range - Array of two integers that specify beginning and end of range.
-   */
   public remove(range: [number, number]): void {
-    // TODO: implement this
+    const rangeToRemove = this.createArrayInRange(range[0], range[1])
+    const newNumbers: number[] = [...this.numbers].filter(value => !rangeToRemove.includes(value))
+
+    this.numbers = [...new Set(newNumbers)]
   }
 
-  /**
-   * Prints out the list of ranges in the range collection
-   */
-  public print(): string {
-    // TODO: implement this
-    return 'string'
+  public print(): void {
+    const ranges: string = this.createRangeCollection(this.numbers)
+      .map(r => `[${r[0]}, ${r[1] + 1})`)
+      .join(' ')
+
+    console.log(ranges)
   }
 }
--- tests/RangeCollection.spec.ts
+++ tests/RangeCollection.spec.ts
@@ -1,25 +1,111 @@
 import 'mocha'
 import * as chai from 'chai'
 import RangeCollection from '../src/RangeCollection'
+import * as sinon from 'sinon'
 
 const { expect } = chai
 
 describe('RangeCollection tests', () => {
+  let rc: RangeCollection
+
+  beforeEach(() => {
+    rc = new RangeCollection()
+  })
+
+  describe('createArrayInRange', () => {
+    it('Should create range with given start and end', () => {
+      const range = rc['createArrayInRange'](1, 10)
+
+      expect(range).to.deep.eq([1, 2, 3, 4, 5, 6, 7, 8, 9])
+    })
+
+    it('Should create range with negative numbers', () => {
+      const range = rc['createArrayInRange'](-10, 1)
+
+      expect(range).to.deep.eq([-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0])
+    })
+
+    it('Should create empty range if start and end is the same', () => {
+      const range = rc['createArrayInRange'](1, 1)
+
+      expect(range).to.deep.eq([])
+    })
+
+    it('Should create empty array in case of end lower than start', () => {
+      const range = rc['createArrayInRange'](1, -1)
+
+      expect(range).to.deep.eq([])
+    })
+  })
+
   describe('add', () => {
-    xit('example test', () => {
-      return
+    it('Should override existing range', () => {
+      rc.add([1, 4])
+      rc.add([1, 4])
+
+      expect(rc['numbers']).to.deep.eq([1, 2, 3])
+    })
+
+    it('Should add new range to existing array and sort correctly', () => {
+      rc.add([5, 10])
+      rc.add([1, 3])
+
+      expect(rc['numbers']).to.deep.eq([1, 2, 5, 6, 7, 8, 9])
+    })
+
+    it('Should not take into account wrong range inputs', () => {
+      rc.add([1, 3])
+      rc.add([5, -10])
+
+      expect(rc['numbers']).to.deep.eq([1, 2])
     })
   })
 
   describe('remove', () => {
-    xit('example test', () => {
-      return
+    it('Should remove given range from current', () => {
+      rc.add([1, 10])
+      rc.remove([1, 8])
+
+      expect(rc['numbers']).to.deep.eq([8, 9])
+    })
+
+    it('Should remove given range that overlaps current range', () => {
+      rc.add([1, 2])
+      rc.remove([-10, 20])
+      rc.remove([-10, 20])
+
+      expect(rc['numbers']).to.deep.eq([])
     })
   })
 
   describe('print', () => {
-    xit('example test', () => {
-      return
+    it('Should print out in correct format', () => {
+      const consoleSpy = sinon.spy(console, 'log')
+
+      rc.add([1, 5])
+      rc.add([10, 20])
+      rc.add([20, 20])
+      rc.add([20, 21])
+      rc.add([2, 4])
+      rc.add([3, 8])
+      rc.remove([10, 10])
+      rc.remove([10, 11])
+      rc.remove([15, 17])
+      rc.remove([3, 19])
+      rc.print()
+
+      expect(consoleSpy.calledOnce).to.eq(true)
+      expect(consoleSpy.getCall(0).args[0]).to.eq('[1, 3) [19, 21)')
+
+      consoleSpy.restore()
+    })
+  })
+
+  describe('createRangeCollection', () => {
+    it('Should create range collection in case given array test', () => {
+      const result = rc['createRangeCollection']([1, 2, 3, 5, 6])
+
+      expect(result).to.deep.eq([[1, 3], [5, 6]])
     })
   })
 })
